bade4eb9b7b35d64c4f8cf07e3ebb3e8f5ae04e1
Trenton Creamer <trentoncreamer@yahoo.com>
2023-06-25 15:11:16 -0700

    Fixed movie bug

--- src/services/movies-service.js
+++ src/services/movies-service.js
@@ -11,10 +11,36 @@ const MOVIES_API = "http://localhost:4000/api/movies";
 // const TUITS_API = `${API_BASE}/tuits`;
 // alert(TUITS_API);
 
+// export const createMovie = async (movie) => {
+//   console.log("Creating a new movie" + JSON.stringify(movie));
+//   console.log(MOVIES_API);
+//   const response = await axios.post(MOVIES_API, movie);
+//   return response.data;
+// };
+
 export const createMovie = async (movie) => {
   console.log("Creating a new movie" + JSON.stringify(movie));
-  console.log(MOVIES_API);
-  const response = await axios.post(MOVIES_API, movie);
+  const existingMovie = await findMovieByImdbId(movie.imdbID);
+
+  if (existingMovie) {
+    console.log("Movie already exists, updating...");
+    return updateMovieByImdbId(movie.imdbID, movie);
+  } else {
+    console.log("Movie does not exist, creating...");
+    const response = await axios.post(MOVIES_API, movie);
+    return response.data;
+  }
+};
+
+export const updateMovieByImdbId = async (imdbId, movie) => {
+  console.log("Updating movie: " + imdbId);
+  const response = await axios.put(`${MOVIES_API}/imdbId/${imdbId}`, movie);
+  return response.data;
+};
+
+export const findMovieByImdbId = async (imdbId) => {
+  console.log("Finding movie by IMDb ID: " + imdbId);
+  const response = await axios.get(`${MOVIES_API}/imdbId/${imdbId}`);
   return response.data;
 };
 
